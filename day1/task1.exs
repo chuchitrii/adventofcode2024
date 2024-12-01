b38c59abc9dc64b4ab86b9a6c3d2e03120235d9c
chuchitrii <chukhinwald@gmail.com>
2024-12-01 21:49:50 +0400

    Use abs()

--- day1/task1.exs
+++ day1/task1.exs
@@ -3,7 +3,7 @@ defmodule Task1 do
     File.stream!("input", :line)
     |> Stream.map(fn line -> 
       String.trim(line)
-      |>String.split("   ") 
+      |> String.split("   ") 
       |> Enum.map(fn x -> String.to_integer(x) end)
       end
     )
@@ -11,18 +11,10 @@ defmodule Task1 do
     |> Enum.reduce({[], []}, fn [n1, n2], {list1, list2} -> {[n1|list1], [n2|list2]} end)
 
   end
-  def distance(a,b) do
-    diff = a - b
-    if diff < 0 do
-      -diff
-    else
-      diff
-    end
-  end
 
   def f({list1, list2}) do
     Enum.zip(Enum.sort(list1), Enum.sort(list2)) 
-    |> Enum.reduce(0, fn {a, b}, acc -> distance(a,b) + acc end)
+    |> Enum.reduce(0, fn {a, b}, acc -> abs(a - b) + acc end)
   end
 end
 
